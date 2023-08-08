328a5832afecdd23b1aa4684f652fb72cddd1160
zhang.ning <qweiopash@qq.com>
2023-08-08 19:52:49 +0800

    Prog2_6_animation

--- main.cpp
+++ main.cpp
@@ -4,56 +4,35 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "Utils.h"
 using namespace std;
 
 #define numVAOs 1
 
 GLuint renderingProgram;
 GLuint vao[numVAOs];
-
-string readFile(const char *filePath) {
-	string content;
-	ifstream fileStream(filePath, ios::in);
-	string line = "";
-	while (!fileStream.eof()) {
-		getline(fileStream, line);
-		content.append(line + "\n");
-	}
-	fileStream.close();
-	return content;
-}
-
-GLuint createShaderProgram() {
-	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-	GLuint vfprogram = glCreateProgram();
-
-	string vertShaderStr = readFile("vertShader.vs");
-	string fragShaderStr = readFile("fragShader.fs");
-	const char *vertShaderSrc = vertShaderStr.c_str();
-	const char *fragShaderSrc = fragShaderStr.c_str();
-
-	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
-	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
-	glCompileShader(vShader);
-	glCompileShader(fShader);
-
-	glAttachShader(vfprogram, vShader);
-	glAttachShader(vfprogram, fShader);
-	glLinkProgram(vfprogram);
-
-	return vfprogram;
-}
+GLuint offsetLoc;
+float x = 0.0f;    // location of triangle on x axis
+float inc = 0.01f; // offset for moving the triangle
 
 void init(GLFWwindow* window) {
-	renderingProgram = createShaderProgram();
+	renderingProgram = Utils::createShaderProgram("vertShader.vs", "fragShader.fs");
 	glGenVertexArrays(numVAOs, vao);
 	glBindVertexArray(vao[0]);
 }
 
 void display(GLFWwindow* window, double currentTime) {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);  // clear the background to black, each time
+
 	glUseProgram(renderingProgram);
-	glPointSize(30.0f);
+
+	x += inc;   // move the triangle along x axis
+	if (x > 1.0f) inc = -0.01f;  // switch to moving the triangle to the left
+	if (x < -1.0f) inc = 0.01f;  // switch to moving the triangle to the right
+	offsetLoc = glGetUniformLocation(renderingProgram, "offset"); // get ptr to "offset"
+	glProgramUniform1f(renderingProgram, offsetLoc, x);  // send value in "x" to "offset"
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
@@ -61,7 +40,7 @@ int main(void) {
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(400, 200, "Chapter 2 - program 5", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(400, 200, "Chapter 2 - program 6", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
 	glfwSwapInterval(1);
